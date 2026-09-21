682c11b975e50f3febfeb38ed582c79b2ac123dc
Simon Stelter <stelter@uni-bremen.de>
2026-09-21 16:29:50 +0200

    refactor(tests): reorganize imports in `test_multi_robot_action_designator` to remove unused dependencies

--- test/coraplex_test/test_designator/test_multi_robot_action_designator.py
+++ test/coraplex_test/test_designator/test_multi_robot_action_designator.py
@@ -33,12 +33,12 @@ from coraplex.robot_plans.actions.composite.transporting import TransportAction
 from coraplex.robot_plans.actions.core.container import OpenAction, CloseAction
 from coraplex.robot_plans.actions.core.misc import DetectAction, MoveToReach
 from coraplex.robot_plans.actions.core.navigation import (
-    PathPlanningNavigateAction,
-)
-from coraplex.robot_plans.actions.core.navigation import (
     NavigateAction,
     LookAtAction,
     ElevatorNavigation,
+)
+from coraplex.robot_plans.actions.core.navigation import (
+    PathPlanningNavigateAction,
 )
 from coraplex.robot_plans.actions.core.pick_up import (
     ReachAction,
@@ -54,9 +54,6 @@ from coraplex.robot_plans.actions.core.robot_body import (
 )
 from coraplex.view_manager import ViewManager
 from giskardpy.utils.utils_for_tests import compare_axis_angle, compare_orientations
-from semantic_digital_twin.adapters.ros.visualization.viz_marker import (
-    VizMarkerPublisher,
-)
 from semantic_digital_twin.callbacks.callback import ModelChangeCallback
 from semantic_digital_twin.datastructures.definitions import (
     TorsoState,
